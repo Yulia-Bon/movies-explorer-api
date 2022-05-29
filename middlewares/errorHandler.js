41d78438ba36f4fd06ba44990614e03723e842b7
Yulia Bonohova <ybonohova@gmail.com>
2022-05-29 17:16:07 +0200

    Исправлен централизованный обработчик, для корретного отображения сообщения об ошибке

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -5,9 +5,9 @@ const errorHandler = (err, req, res, next) => {
     return res.status(400).send({ message: err.details.get('body').details[0].message });
   }
   if (err.status) {
-    return res.status(err.status).send({ message: err.message });
+    return res.status(err.status).send(err.message);
   }
-  res.status(500).send({ message: err.message });
+  res.status(500).send(err.message);
   next();
   return 0;
 };
